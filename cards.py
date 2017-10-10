15e2885a6d43b6369ceaf3eb0600a5b9c06b0878
edeane <edward.deane.ii@gmail.com>
2017-10-09 20:19:36 -0600

    basic

--- cards.py
+++ cards.py
@@ -2,8 +2,9 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2.cv2 as cv2
-from skimage.filters import threshold_adaptive
+from cv2 import cv2
+from skimage.filters import threshold_local, threshold_adaptive
+import os
 
 def order_points(pts):
 	rect = np.zeros((4, 2), dtype = "float32")
@@ -32,53 +33,82 @@ def four_point_transform(image, pts):
 	M = cv2.getPerspectiveTransform(rect, dst)
 	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 	return warped
+    
+def proccess_card(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = threshold_adaptive(image, 251, offset=10)
+    image = image.astype("uint8") * 255
+    dim = (245, 343)
+    image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+    return image
 
-def exploring():
-    all_cards = cv2.imread('data/sample/all.jpg')
-    print(type(all_cards))
-    print(all_cards.size)
-    print(all_cards.shape)
-    print(all_cards[0])
-    plt.imshow(all_cards)
-    plt.show()
-    b, g, r = cv2.split(all_cards)
-    plt.imshow(b, cmap='gray')
-    plt.show()
-    all_cards_merged = cv2.cvtColor(all_cards, cv2.COLOR_BGR2RGB)
-    plt.imshow(all_cards_merged)
-    plt.show()
-    
-    
-if __name__ == '__main__':
-    image = cv2.imread('data/sample/7.jpg')
-    r = 500 / image.shape[1]
-    dim = (500, int(image.shape[0] * r))
-    ratio = image.shape[0] / 500.0
-    orig = image.copy()
-    image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)   
+def detect_cards(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     edged = cv2.Canny(gray, 75, 200)
-    #cv2.imshow("Image", image)
-    #cv2.imshow("Edged", edged)
+    # cv2.imshow('edged', edged)
     (_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
-    screenCnts = []
-    for c in cnts:
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)
+    return cnts
+    
+    
+def create_labeled_images():
+    image = cv2.imread('data/sample/all_cards.jpg')
+    cnts = detect_cards(image)[:120]
+    for i, c in enumerate(cnts):
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.02 * peri, True)
         if len(approx) == 4:
-            screenCnts.append(approx)
-    cv2.drawContours(image, cnts, -1, (0, 255, 0), 2)
-    cv2.imshow("green", image)
-    for i in range(len(screenCnts)):
-        warped = four_point_transform(image, screenCnts[i].reshape(4, 2))
-        warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-        warped = threshold_adaptive(warped, 251, offset = 10)
-        warped = warped.astype("uint8") * 255
-        cv2.imshow('warp {}'.format(i), warped)
+            warped = four_point_transform(image, approx.reshape(4, 2))
+            card = proccess_card(warped)
+            cv2.imwrite('data/labeled_2/card_{}.png'.format(i), card)
+        
+def get_labled_images():
+    labled = {}
+    loc = 'data/labeled_2/'
+    for file in os.listdir(loc):
+        if file.endswith('.png'):
+            warped = cv2.imread(loc + file)
+            card = proccess_card(warped)
+            labled[file.split('.')[0]] = card
+    return labled
+
+def calc_difference(image_1, image_2):
+    image_1 = cv2.GaussianBlur(image_1,(5,5),5)
+    image_2 = cv2.GaussianBlur(image_2,(5,5),5)
+    diff = cv2.absdiff(image_1, image_2)
+    diff = cv2.GaussianBlur(diff,(5,5),5)    
+    flag, diff_1 = cv2.threshold(diff, 200, 255, cv2.THRESH_BINARY)
+    return np.sum(diff)
     
+
+def image_difference():
+    labled = get_labled_images()
+    image = cv2.imread('data/sample/all_cards.jpg')
+    cnts = detect_cards(image)[:10]
+    results = {}
+    for i, c in enumerate(cnts):
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:
+            input_card = four_point_transform(image, approx.reshape(4, 2))
+            input_card = proccess_card(input_card)
+            (h, w) = input_card.shape[:2]
+            center = (w / 2, h / 2)
+            M = cv2.getRotationMatrix2D(center, 180, 1.0)
+            input_card_rotated = cv2.warpAffine(input_card, M, (w, h))
+            diffs = {}
+            for label_name, label_card in labled.items():
+                diff_1 = calc_difference(input_card, label_card)
+                diff_2 = calc_difference(input_card_rotated, label_card)  
+                diffs[label_name] = min(diff_1, diff_2)
+            results[i] = min(diffs, key=diffs.get)
+            cv2.imshow('input_card_{}'.format(i), input_card)
+    return results
     
+if __name__ == '__main__':
+    results = image_difference()
+    print(results)
     
     
     
